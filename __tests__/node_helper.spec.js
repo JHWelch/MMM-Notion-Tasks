b70b3d86c94f161e5cad9e763c1240e7ed854778
Jordan Welch <jordanhwelch@gmail.com>
2025-10-12 15:39:02 -0500

    Add node_helper call

--- __tests__/node_helper.spec.js
+++ __tests__/node_helper.spec.js
@@ -1,3 +1,5 @@
+const { Client } = require('@notionhq/client');
+
 beforeAll(() => {
   require('../__mocks__/logger');
 });
@@ -15,6 +17,44 @@ describe('socketNotificationReceived', () => {
       helper.socketNotificationReceived('NOT-Notion-Tasks-FETCH', {});
 
       expect(helper.sendSocketNotification).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('notification matches MMM-Notion-Tasks-FETCH', () => {
+    it('fetches tasks from Notion', async () => {
+      const query = jest.fn();
+      Client.mockImplementation(() => ({
+        dataSources: { query },
+      }));
+
+      query.mockImplementation(() => Promise.resolve({ results: [
+        {
+          object: 'page',
+          id: 'page-id',
+          properties: {
+            Name: { title: [{ text: { content: 'Task 1' } }] },
+            Status: { select: { name: 'In Progress' } },
+          },
+        },
+        {
+          object: 'page',
+          id: 'page-id-2',
+          properties: {
+            Name: { title: [{ text: { content: 'Task 2' } }] },
+            Status: { select: { name: 'Completed' } },
+          },
+        },
+      ] }));
+
+      await helper.socketNotificationReceived('MMM-Notion-Tasks-FETCH', {
+        notionToken: 'secret-token',
+        databaseId: 'database-id',
+      });
+
+      expect(helper.sendSocketNotification).toHaveBeenCalledWith('MMM-Notion-Tasks-DATA', {tasks: [
+        { id: 'page-id', name: 'Task 1', status: 'In Progress' },
+        { id: 'page-id-2', name: 'Task 2', status: 'Completed' },
+      ]});
     });
   });
 });

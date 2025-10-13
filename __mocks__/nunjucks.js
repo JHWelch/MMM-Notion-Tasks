621223330f4d9de5de4b96e3f29f5d9f71703f77
Jordan Welch <jordanhwelch@gmail.com>
2025-10-12 23:33:14 -0500

    Add name filter to assignee

--- __mocks__/nunjucks.js
+++ __mocks__/nunjucks.js
@@ -5,5 +5,6 @@ nunjucksEnvironment.addFilter(
   'translate',
   (str, variables) => nunjucks.runtime.markSafe(translate(str, variables)),
 );
+nunjucksEnvironment.addFilter('name', (str) => str);
 
 module.exports = nunjucks;
